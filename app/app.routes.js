c7a95a894a8a7d4ab0213b8a33f61907ba5e01b6
Richard Plotkin <richardjplotkin@gmail.com>
2016-07-25 16:31:14 -0700

    add abstract authenticated state

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,4 +1,17 @@
-export default function routing($locationProvider, $urlRouterProvider) {
+export default function routing($locationProvider, $urlRouterProvider, $stateProvider, $resourceProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('login');
+
+  $stateProvider.state('authenticated', {
+    abstract: true,
+    views: {
+      nav: {
+        template: '<div nav></div>'
+      },
+      main: {
+        template: '<ui-view/>'
+      }
+    }
+  });
+
 }
