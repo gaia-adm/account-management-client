da6cb6f86432351f487d31a715faa49e860c2ea1
boriska70@gmail.com <boriska70@gmail.com>
2016-10-18 17:53:27 +0300

    Add conditional links to Gaia UI and GAU

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,7 +1,16 @@
 export default class HomeController {
 
-  constructor(AuthUser, UserResource){
-    'ngInject';
-    this.user = UserResource.get({id: 'self'});
-  };
+    constructor(AuthUser, UserResource) {
+        'ngInject';
+        this.user = UserResource.get({id: 'self'});
+        this.envi = this.prepareEnviDetails();
+    };
+
+    prepareEnviDetails() {
+        var myEnvi = new Object();
+        myEnvi.proto = location.protocol;
+        location.hostname.startsWith('acm') ? myEnvi.srv = location.hostname.substring(5) : myEnvi.srv = location.hostname;
+        myEnvi.proto === 'https:' ? myEnvi.servicesPort = 444 : myEnvi.servicesPort = 88;
+        return myEnvi;
+    }
 }
